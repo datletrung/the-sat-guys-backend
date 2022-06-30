9ad6acc6143090777687f1372e700d32c5db129e
datletrung <letrungcaotung@gmail.com>
2022-06-30 10:29:59 -0300

    Enable CORS on API 'ping'

--- amplify/backend/function/ping/src/index.py
+++ amplify/backend/function/ping/src/index.py
@@ -2,14 +2,14 @@ import json
 
 def handler(event, context):
     return {
-        'statusCode': 200,
-        'body': json.dumps({
-                            "status": True,
-                             "headers": {
-                                "Access-Control-Allow-Headers": "Content-Type",
-                                "Access-Control-Allow-Origin": "*",
-                                "Access-Control-Allow-Methods": "OPTIONS,POST,GET"
-                            },
-                            "message": "pong",
-                })
-    }
+            'statusCode': 200,
+            'headers': {
+                'Access-Control-Allow-Headers': 'Content-Type',
+                'Access-Control-Allow-Origin': '*',
+                'Access-Control-Allow-Methods': 'OPTIONS,POST,GET'
+            },
+            'body': json.dumps({
+                                "status":True,
+                                "data":"pong",
+                    })
+        }
